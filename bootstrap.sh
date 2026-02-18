c7397630cd01575e8a46b784749323629332b782
Daniel Bodart <dan@bodar.com>
2026-02-18 14:45:43 +0000

    Fix LFS so it actually works

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,7 +11,7 @@ http() { curl --progress-bar "$@" || wget -qO- "$@"; }
 
 [[ -f "$MISE_INSTALL_PATH" ]] || http https://mise.run | sh
 git -C "$SCRIPT_DIR" submodule update --init --recursive --quiet
-command -v git-lfs &>/dev/null && git lfs install --local
+command -v git-lfs &>/dev/null && git lfs install --local && git -C "$SCRIPT_DIR" lfs pull
 mise trust --quiet "$SCRIPT_DIR"
 mise install
 eval "$(mise env)"
